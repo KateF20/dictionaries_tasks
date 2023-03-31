8c8a6957f8baaab69530d5e169dfde521adcfc04
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-03-31 22:25:10 +0200

    first commit

---
+++ programmer_dict.py
@@ -0,0 +1,9 @@
+dictionary = [input() for i in range(int(input()))] # get input number of lines of a dict
+words = [input().lower() for i in range(int(input()))] # get input number of lines of lowered words to search
+
+# dict comprehension to make a dict from a list, splitting each element by ': ' and lowering the keys
+my_dict = {i.split(': ')[0].lower(): i.split(': ')[1] for i in dictionary}
+
+# print a value from the dict
+for word in words:
+    print(my_dict[word] if word in my_dict.keys() else 'Не найдено')
